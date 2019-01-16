30ad2b8ee06a3ea3b5134e2c5d6f1178c5ff4e06
dickyang <dickyang@celeschain.io>
2019-01-16 19:18:23 +0800

    test

--- celesos.system/include/celesos.system/celesos.system.hpp
+++ celesos.system/include/celesos.system/celesos.system.hpp
@@ -38,7 +38,7 @@
 // singing ticker sep（唱票间隔期，每隔固定时间进行唱票）
 #define SINGING_TICKER_SEP BP_COUNT * 6 * 60
 
-#define DBP_ACTIVE_SEP 6 * 24 * 60 * 60 * 2
+#define DBP_ACTIVE_SEP 5 * 24 * 60 * 60 * 2
 
 // #else
 
