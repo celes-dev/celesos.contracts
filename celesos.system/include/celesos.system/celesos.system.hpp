65cfaa4405fc5dbf9bcb0d89c910478f21798168
dickyang <dickyang@celeschain.io>
2019-02-15 10:11:13 +0800

    modify param

--- celesos.system/include/celesos.system/celesos.system.hpp
+++ celesos.system/include/celesos.system/celesos.system.hpp
@@ -30,15 +30,13 @@
 // number of bp,BP个数
 #define BP_COUNT 21
 // when the bp count is ok cycle for this number,the active the network(主网启动条件，BP个数达标轮数）
-// #define ACTIVE_NETWORK_CYCLE 24
-#define ACTIVE_NETWORK_CYCLE 1
+#define ACTIVE_NETWORK_CYCLE 24
 // reward get min（if smaller than this number，you can't get the reward）最小奖励领取数，低于此数字将领取失败
 #define REWARD_GET_MIN 1000000
 // get reward time sep(奖励领取间隔时间，单位：秒）
 #define REWARD_TIME_SEP 6 * 60 * 60 * uint64_t(1000000)
 // singing ticker sep（唱票间隔期，每隔固定时间进行唱票）
-// #define SINGING_TICKER_SEP BP_COUNT * 6 * 60
-#define SINGING_TICKER_SEP BP_COUNT * 60
+#define SINGING_TICKER_SEP BP_COUNT * 6 * 60
 
 #define DBP_ACTIVE_SEP 1 * 24 * 60 * 60 * 2
 
